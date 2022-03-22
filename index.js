1c7c68db603741134386b52e590dec0212df107f
fed1k <allamurotovcoder@mail.ru>
2022-03-22 10:50:01 +0500

    Replace functions with arrow functions and using modules

---
+++ index.js
@@ -0,0 +1,97 @@
+import {addFunction, adder} from './modules/functions.js'
+import {add, form, container, round, list, navAdd, btn, body, h1, contact, logo, contactSection} from './modules/references.js'
+export { main}
+
+// Clock with date and hour
+const dateSpan = document.querySelector('#span');
+const dateShower = ()=>{
+  const date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+  dateSpan.textContent = `${hour}:${minute}:${second}`;
+}
+setInterval(dateShower, 1000);
+
+// Add books to the list
+const main = [];
+
+add.addEventListener('click', adder);
+const inputAuthor = document.querySelector('#author');
+inputAuthor.addEventListener('keyup', e =>{
+  if(e.keyCode === 13){
+    adder();
+  }
+});
+
+// Taking information from the local storage when page loads
+window.addEventListener('load', ()=>{
+  list.classList.add('addUI');
+  let something = localStorage.getItem('list');
+  let parsing = JSON.parse(something);
+  for(let i=0;i<parsing.length;i++){
+    if(parsing[i]){
+      main.push(parsing[i]);
+      // Create book every list container
+      let div = document.createElement('div');
+      const span1 = document.createElement('span');
+      const remove = document.createElement('button');
+      remove.textContent = 'Remove';
+      remove.className = 'remove';
+      div.className = 'listDiv';
+      container.appendChild(div);
+      div.append(span1, remove);
+      container.style.display = 'flex';
+      span1.textContent = `"${parsing[i].title}"  by  ${parsing[i].author}`;
+      remove.addEventListener('click', ()=>{
+        delete parsing[i]
+        localStorage.setItem('list', JSON.stringify(parsing))
+        div.style.display = 'none';
+      })  
+    }
+  }
+})
+
+// Navigation event listeners
+navAdd.addEventListener('click', ()=>{
+  form.style.display = 'flex';
+  contactSection.style.display = 'none';
+  h1.textContent = 'Add a new book';
+  navAdd.classList.add('addUI');
+  list.classList.remove('addUI');
+  contact.classList.remove('addUI');
+  container.style.display = 'none';
+})
+
+list.addEventListener('click', addFunction)
+
+contact.addEventListener('click', ()=>{
+  form.style.display = 'none';
+  h1.textContent = 'Contact information';
+  contactSection.style.display = 'flex';
+  contact.classList.add('addUI');
+  navAdd.classList.remove('addUI');
+  list.classList.remove('addUI');
+  container.style.display = 'none';
+})
+
+logo.addEventListener('click', addFunction);
+
+// Night mode button functionality
+let day = 1;
+btn.addEventListener('click', ()=>{
+  round.classList.toggle('toggle');
+  body.classList.toggle('body-night-mode');
+  day++;
+  localStorage.setItem('night', day);
+})
+
+window.addEventListener('load', ()=>{
+  let isDay = localStorage.getItem('night');
+  let getdata = JSON.parse(isDay);
+  day = getdata;
+  if(getdata % 2 == 0){
+    body.classList.add('body-night-mode');
+    round.classList.add('toggle');
+  }
+})
---
+++ modules/functions.js
@@ -0,0 +1,46 @@
+const addFunction = ()=>{
+  form.style.display = 'none';
+  contactSection.style.display = 'none';
+  h1.textContent = 'All awesome books';
+  list.classList.add('addUI');
+  contact.classList.remove('addUI');
+  navAdd.classList.remove('addUI');
+  container.style.display = 'flex';
+}
+
+const adder = (e)=>{
+  if (form.title.value && form.author.value) {
+    const obj = new Whole(form.title.value, form.author.value);
+    main.push(obj);
+    let s = localStorage.setItem('list', JSON.stringify(main));      
+    // Create book every list container
+    const div = document.createElement('div');
+    const span1 = document.createElement('span');
+    const remove = document.createElement('button');
+    div.className = 'listDiv';
+    // bookslistContainer = div;
+    remove.textContent = 'Remove';
+    remove.className = 'remove';
+    container.appendChild(div);
+    div.append(span1, remove);
+    container.style.display = 'none';
+    // Remove books from the list
+    remove.addEventListener('click', () => {
+      for(let i=0;i<main.length;i++){
+        div.style.display = 'none';
+        delete main[i];
+        localStorage.setItem('list', JSON.stringify(main));
+      }
+    })
+    span1.textContent = `"${obj.title}"  by  ${obj.author}`;
+    form.title.value = null;
+    form.author.value = null;  
+  } else {
+    e.preventDefault();
+  }
+}
+
+export {addFunction, adder}
+import { form, container, list, navAdd, bookslistContainer, h1, contact, contactSection } from './references.js'
+import { main } from '../index.js'
+import { Whole } from './class.js'
